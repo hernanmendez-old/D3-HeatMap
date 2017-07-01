2f2a19df2dd3734cf798275968e31c1011f27392
Hernan Mendez <hernan21112001@gmail.com>
2017-07-01 18:06:57 -0400

    colors and tooltip added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-function callback(data){
+function callback(Data){
 /////////////////////////////////////
-var data = data.data.monthlyVariance;
+var data = Data.data.monthlyVariance;
 /////////////////////////////////////
 
-var height=300;
+var height=430;
 var width=800;
 var barHeight=height/12;
 var barWidth=width/Math.ceil(data.length/12);
+var tooltip=document.getElementById('tooltip');
 
 d3.select('#main')
   .append('svg')
-    .attr('width',1000)
-    .attr('height',500)
+    .attr('width',width+200)
+    .attr('height',height+200)
   .append('g')
     .attr('width',width)
     .attr('height',height)
@@ -27,6 +28,9 @@ var map=d3.select('.map');
 var xPos=d3.scaleLinear()
     .domain([data[0].year,data[data.length-1].year])
     .range([0,width-barWidth])
+var color=d3.scaleQuantile()
+    .domain([-7,-5,-3,-1,0,1])
+    .range(['#0047b3','#66b3ff','#99ffcc','#ffff66','#ffb366','#ff1a1a'])
 
 map.selectAll('rect')
     .data(data)
@@ -35,6 +39,22 @@ map.selectAll('rect')
     .attr('y',function(d){return barHeight*(d.month-1)})
     .attr('width',barWidth)
     .attr('height',barHeight)
-    .style('fill','black')
+    .style('fill',function(d){return color(d.variance)})
+    .on('mouseover',function(d){
+
+        tooltip.style.display="block";
+        tooltip.style.top=barHeight*(d.month)+100+'px';
+        if(window.innerWidth>1000) tooltip.style.left=parseInt(xPos(d.year),10)+(window.innerWidth-1000)/2+barWidth+100+'px';
+        else tooltip.style.left=parseInt(xPos(d.year),10)+100+barWidth+'px';
+
+        tooltip.innerHTML='year: '+d.year+'<br/>Temperature Variance: '+d.variance+'<br/>Temperature: '+(Data.data.baseTemperature+d.variance);
+
+    })
+    .on('mouseout',function(){
+
+        tooltip.style.display="none";
+
+    })
+
 }
 axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json').then(callback);
